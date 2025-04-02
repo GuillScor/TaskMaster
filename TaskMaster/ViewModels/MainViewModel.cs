2205ad2340e246c657558e1fb6abd00083d18e5a
RaggedBean <149085454+RaggedBean@users.noreply.github.com>
2025-04-02 10:15:22 +0200

    Initialisation MVVM
    Creation structure de base MVVM dans app MAUI par defaut - View et ViewModel seulemet

---
+++ TaskMaster/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TaskMaster.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int count;
+
+    private string counterText = "Click me";
+    public string CounterText
+    {
+        get => counterText;
+        set => SetProperty(ref counterText, value);
+    }
+
+    public MainViewModel()
+    {
+        CounterText = "Click me"; // Valeur initiale
+    }
+
+    [RelayCommand]
+    private void IncrementCounter()
+    {
+        Count++;
+        CounterText = Count == 1 ? $"Clicked {Count} time" : $"Clicked {Count} times";
+        SemanticScreenReader.Announce(CounterText); // Accessibilité
+    }
+}
